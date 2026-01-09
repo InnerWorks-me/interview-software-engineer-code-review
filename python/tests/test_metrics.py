37fcb26b7048b1d7fff92c20e20faf83a90393c0
Samson Ventura-Danziger <samson@innerworks.me>
2026-01-09 10:46:13 +0000

    python: apply async changes

--- python/tests/test_metrics.py
+++ python/tests/test_metrics.py
@@ -1,6 +1,7 @@
 import pytest
 from app.metrics import ingest_metrics
 
-def test_ingest_metrics():
+@pytest.mark.asyncio
+async def test_ingest_metrics():
     with pytest.raises(NotImplementedError):
-        ingest_metrics('{"project_id": "abc123", "metrics": {}}')
+        await ingest_metrics('{"project_id": "abc123", "metrics": {}}')
